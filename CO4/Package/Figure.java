3337aa8c93bf74037b8afa5095fae41ea4022751
Vivek S <71586511+viveks55@users.noreply.github.com>
2022-07-14 20:34:11 +0530

    Source Code

---
+++ CO4/Package/Figure.java
@@ -0,0 +1,13 @@
+import Graphics.*;
+import java.io.*;
+import java.util.*;
+class Figure{
+	public static void main(String [] args){
+		Rectangle1 r=new Rectangle1();
+		r.getdata();
+		r.area();
+		Student1 s=new Student1();
+		s.get();
+	s.show();
+	}
+}
